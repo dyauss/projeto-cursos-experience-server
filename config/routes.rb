7b73da8222ca0c938b8009705b224d999984cf34
dyauss <thandy_97@hotmail.com>
2023-04-21 01:14:47 -0300

    curso api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  # resources :courses
 
   get '/users', to: 'api/v1/users#index'
   get '/users/:id', to: 'api/v1/users#getone'
   delete '/users/:id', to: 'api/v1/users#destroy'
   put '/users/:id', to: 'api/v1/users#update'
+
+  get '/courses', to: 'api/v1/courses#index'
+  post '/courses', to: 'api/v1/courses#create'
+  delete '/courses/:id', to: 'api/v1/courses#destroy'
+  put '/courses/:id', to: 'api/v1/courses#update'
 
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
